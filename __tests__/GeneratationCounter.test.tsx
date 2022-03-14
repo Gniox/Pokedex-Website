3c572a244ee0dc6c3d2ffcdfdabeef433af8f8fc
Gniox <gniox15@gmail.com>
2022-03-14 12:56:07 -0500

    changed jest.config.ts back to .json

--- __tests__/GeneratationCounter.test.tsx
+++ __tests__/GeneratationCounter.test.tsx
@@ -1,58 +1,11 @@
 import React from "react";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
+// import { rest } from "msw";
+// import { setupServer } from "msw/node";
 import GenerationCounter from "../components/GenerationCounter";
 import { render, screen, act } from "@testing-library/react";
 import fetchMock from "jest-fetch-mock";
-// import '@testing-library/jest-dom/extend-expect';
 
 require("jest-fetch-mock").enableMocks();
-
-const server = setupServer(
-  rest.get("https://https://pokeapi.co/api/v2/generation", (req, res, ctx) => {
-    return res(
-      ctx.json({
-        count: 8,
-        next: null,
-        previous: null,
-        results: [
-          {
-            name: "generation-i",
-            url: "https://pokeapi.co/api/v2/generation/1/",
-          },
-          {
-            name: "generation-ii",
-            url: "https://pokeapi.co/api/v2/generation/2/",
-          },
-          {
-            name: "generation-iii",
-            url: "https://pokeapi.co/api/v2/generation/3/",
-          },
-          {
-            name: "generation-iv",
-            url: "https://pokeapi.co/api/v2/generation/4/",
-          },
-          {
-            name: "generation-v",
-            url: "https://pokeapi.co/api/v2/generation/5/",
-          },
-          {
-            name: "generation-vi",
-            url: "https://pokeapi.co/api/v2/generation/6/",
-          },
-          {
-            name: "generation-vii",
-            url: "https://pokeapi.co/api/v2/generation/7/",
-          },
-          {
-            name: "generation-viii",
-            url: "https://pokeapi.co/api/v2/generation/8/",
-          },
-        ],
-      })
-    );
-  })
-);
 
 describe("GenerationCounter", () => {
   beforeEach(() => {
@@ -106,9 +59,8 @@ describe("GenerationCounter", () => {
       render(<GenerationCounter />);
     });
 
-    const expected = "generation-i";
-    const list = await screen.findByText("generation-i");
+    const list = screen.getByRole("list-item");
 
-    // expect(list.textContent).toBe(expected);
+    expect(list).toBe(8);
   });
 });
