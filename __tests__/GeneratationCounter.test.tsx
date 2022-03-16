c2aefeee962b4a9dab60057a31b1fdf7b0057ce5
Gniox <gniox15@gmail.com>
2022-03-16 08:45:45 -0500

    moved some tests around in GenCounter

--- __tests__/GeneratationCounter.test.tsx
+++ __tests__/GeneratationCounter.test.tsx
@@ -305,85 +305,85 @@ describe("Down Arrow", () => {
         expect(element.textContent).toBe(generation[index]);
       });
     });
-  });
-  it("Stays within constraints of generation list", async () => {
-    const generation = [
-      "generation-i",
-      "generation-ii",
-      "generation-iii",
-      "generation-iv",
-      "generation-v",
-      "generation-vi",
-      "generation-vii",
-      "generation-viii",
-    ];
+    it("Stays within constraints of generation list", async () => {
+      const generation = [
+        "generation-i",
+        "generation-ii",
+        "generation-iii",
+        "generation-iv",
+        "generation-v",
+        "generation-vi",
+        "generation-vii",
+        "generation-viii",
+      ];
 
-    fetchMock.mockResponseOnce(
-      JSON.stringify({
-        count: 8,
-        next: null,
-        previous: null,
-        results: [
-          {
-            name: "generation-i",
-            url: "https://pokeapi.co/api/v2/generation/1/",
-          },
-          {
-            name: "generation-ii",
-            url: "https://pokeapi.co/api/v2/generation/2/",
-          },
-          {
-            name: "generation-iii",
-            url: "https://pokeapi.co/api/v2/generation/3/",
-          },
-          {
-            name: "generation-iv",
-            url: "https://pokeapi.co/api/v2/generation/4/",
-          },
-          {
-            name: "generation-v",
-            url: "https://pokeapi.co/api/v2/generation/5/",
-          },
-          {
-            name: "generation-vi",
-            url: "https://pokeapi.co/api/v2/generation/6/",
-          },
-          {
-            name: "generation-vii",
-            url: "https://pokeapi.co/api/v2/generation/7/",
-          },
-          {
-            name: "generation-viii",
-            url: "https://pokeapi.co/api/v2/generation/8/",
-          },
-        ],
-      })
-    );
+      fetchMock.mockResponseOnce(
+        JSON.stringify({
+          count: 8,
+          next: null,
+          previous: null,
+          results: [
+            {
+              name: "generation-i",
+              url: "https://pokeapi.co/api/v2/generation/1/",
+            },
+            {
+              name: "generation-ii",
+              url: "https://pokeapi.co/api/v2/generation/2/",
+            },
+            {
+              name: "generation-iii",
+              url: "https://pokeapi.co/api/v2/generation/3/",
+            },
+            {
+              name: "generation-iv",
+              url: "https://pokeapi.co/api/v2/generation/4/",
+            },
+            {
+              name: "generation-v",
+              url: "https://pokeapi.co/api/v2/generation/5/",
+            },
+            {
+              name: "generation-vi",
+              url: "https://pokeapi.co/api/v2/generation/6/",
+            },
+            {
+              name: "generation-vii",
+              url: "https://pokeapi.co/api/v2/generation/7/",
+            },
+            {
+              name: "generation-viii",
+              url: "https://pokeapi.co/api/v2/generation/8/",
+            },
+          ],
+        })
+      );
 
-    await act(async () => {
-      render(<GenerationCounter />);
-    });
+      await act(async () => {
+        render(<GenerationCounter />);
+      });
 
-    const downButton = screen.getByRole("button", {
-      name: "down",
-    });
-    const upButton = screen.getByRole("button", {
-      name: "up",
-    });
-    const list = screen.getByRole("list");
-    const { getAllByRole } = within(list);
-    const items = getAllByRole("listitem");
+      const downButton = screen.getByRole("button", {
+        name: "down",
+      });
+      const upButton = screen.getByRole("button", {
+        name: "up",
+      });
+      const list = screen.getByRole("list");
+      const { getAllByRole } = within(list);
+      const items = getAllByRole("listitem");
 
-    items.forEach((element, index) =>
-      expect(element.textContent).toBe(generation[index])
-    );
+      items.forEach((element, index) =>
+        expect(element.textContent).toBe(generation[index])
+      );
 
-    fireEvent.click(upButton);
+      fireEvent.click(upButton);
 
-    const newItems = getAllByRole("listitem");
+      const newItems = getAllByRole("listitem");
 
-    newItems.forEach((element, index) => {
-      expect(element.textContent).toBe(generation[index]);
+      newItems.forEach((element, index) => {
+        expect(element.textContent).toBe(generation[index]);
+      });
     });
   });
 });
