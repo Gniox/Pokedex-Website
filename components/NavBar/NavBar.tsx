d3640ef324e323aa738d84f15e9e4ff3c727a77f
Gniox <gniox15@gmail.com>
2022-03-16 14:26:07 -0500

    latest commit

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
 
-//TODO: make li display:inline, for 
+const Div = styled.div`
+  display: flex;
+  align-items: center;
+  justify: center;
+  flex-direction: column;
+`;
+const StyledList = styled.ul`
+  list-style-type: none;
+`;
+
+const StyledListItem = styled.li`
+  display: inline;
+`;
+
+//TODO: make li display:inline, for
 //      horizontal alignment
 const NavBar: React.FC = () => {
-  return <></>;
+  type pokeSpecies = {
+    name: string;
+    url: string;
+  };
+
+  // interface pokeList = {
+  //   abilities:
+  // }
+
+  const [pokeList, setPokeList] = React.useState<pokeSpecies[]>([]);
+  const [listShown, setListShown] = React.useState<pokeSpecies[]>([]);
+  const [limit, setLimit] = React.useState(6);
+  const [offset, setOffSet] = React.useState(0);
+
+  useEffect(() => {
+    let isMounted = true;
+    fetch("https://pokeapi.co/api/v2/generation/1/")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        if (isMounted) {
+          setPokeList(data.pokemon_species);
+          setListShown(data.pokemon_species.slice(offset, limit));
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data: " + error);
+      });
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return (
+    <Div>
+      <StyledList>
+        {listShown.map((item, index) => {
+          return <StyledListItem key={uuidv4()}>{index+1}</StyledListItem>;
+        })}
+      </StyledList>
+    </Div>
+  );
 };
 
 export default NavBar;
