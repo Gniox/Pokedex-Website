176ddee53d494ec152525034cf851736fd111b4b
Gniox <gniox15@gmail.com>
2022-03-19 11:26:46 -0500

    saving changes

--- functions/sortPokemon.ts
+++ functions/sortPokemon.ts
@@ -3,7 +3,11 @@ type pokeSpecies = {
   url: string;
 };
 
+//Sorts array of PokeSpecies by url(number)
 export function sortPokemon(pokemon: pokeSpecies[]): pokeSpecies[] {
+  
+
+
   const sortedPokemon = pokemon;
 
   return sortedPokemon;
